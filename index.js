81314c9d4b9b653616b803743b7c06073c6ca64d
Erick Merchant <erickmerchant@gmail.com>
2020-05-25 13:28:45 -0400

    Add package.json, LICENSE, and config

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "rules": {
+    "eqeqeq": [
+      "error",
+      "always",
+      {
+        "null": "never"
+      }
+    ],
+    "no-else-return": "error",
+    "no-useless-escape": "error",
+    "no-useless-return": "error",
+    "no-delete-var": "error",
+    "no-undef": "error",
+    "no-unused-vars": [
+      "error",
+      {
+        "vars": "all",
+        "args": "none"
+      }
+    ],
+    "handle-callback-err": "error",
+    "no-path-concat": "error",
+    "no-var": "error",
+    "prefer-arrow-callback": "error",
+    "prefer-const": "error",
+    "prefer-template": "error"
+  }
+}
