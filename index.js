dac47ce59b55921b2a6ba9a4ed6c309fbdefc490
Erick Merchant <erickmerchant@gmail.com>
2020-05-26 20:41:09 -0400

    More rules, a placeholder test, and dev deps

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  extends: "eslint:recommended",
   parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2020,
@@ -12,6 +13,17 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
+
+    // imports and exports
+    "no-duplicate-imports": "error",
+    "no-useless-rename": "error",
+
+    // control
+    "default-case-last": "error",
+    "no-else-return": "error",
+    "no-lonely-if": "error",
+
+    // comparison
     eqeqeq: [
       "error",
       "always",
@@ -19,11 +31,50 @@ module.exports = {
         null: "never",
       },
     ],
-    "no-else-return": "error",
-    "no-useless-escape": "error",
+    "no-mixed-operators": "error",
+
+    // numbers
+    "no-floating-decimal": "error",
+    "no-loss-of-precision": "error",
+    radix: "error",
+
+    // strings
+    "no-multi-str": "error",
+    "no-octal-escape": "error",
+    "no-template-curly-in-string": "error",
+    "no-useless-concat": "error",
+    "prefer-template": "error",
+
+    // regex
+    "no-useless-backreference": "error",
+    "prefer-regex-literals": "error",
+
+    // array
+    "array-callback-return": "error",
+    "no-array-constructor": "error",
+
+    // functions
+    "no-loop-func": "error",
+    "prefer-arrow-callback": "error",
+
+    // parameters
+    "default-param-last": "error",
+
+    // return
+    "no-return-assign": "error",
+    "no-return-await": "error",
     "no-useless-return": "error",
-    "no-delete-var": "error",
-    "no-undef": "error",
+
+    // errors
+    "no-throw-literal": "error",
+
+    // promises
+    "no-await-in-loop": "error",
+    "prefer-promise-reject-errors": "error",
+
+    // vars
+    "no-var": "error",
+    "prefer-const": "error",
     "no-unused-vars": [
       "error",
       {
@@ -31,11 +82,27 @@ module.exports = {
         args: "none",
       },
     ],
+
+    // misc
+    "require-atomic-updates": "error",
+    "symbol-description": "error",
+
+    // old school
+    "no-caller": "error",
+    "no-eval": "error",
+    "no-extend-native": "error",
+    "no-extra-bind": "error",
+    "no-implied-eval": "error",
+    "no-new-func": "error",
+    "no-new-wrappers": "error",
+    "no-proto": "error",
+    "no-script-url": "error",
+    "no-sequences": "error",
+    "no-useless-call": "error",
+    "no-void": "error",
+
+    // deprecated (node)
     "handle-callback-err": "error",
     "no-path-concat": "error",
-    "no-var": "error",
-    "prefer-arrow-callback": "error",
-    "prefer-const": "error",
-    "prefer-template": "error",
   },
 };
